e926fe112b52714eab87ab8983d4ce0160c62e02
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-10-29 10:57:08 +0200

    Optimization update:
    -Removed layout and objectFit Props: Replaced layout="fill" and objectFit="cover" with width and height props and added style={{ objectFit: 'cover' }}.
    -Updated sizes Attribute: Corrected syntax to ensure responsiveness on different screen sizes.

--- app/components/RecipeCard.jsx
+++ app/components/RecipeCard.jsx
@@ -32,9 +32,11 @@ const RecipeCard = ({ recipe: { _id, title, images, prep, cook, servings, tags =
           <Image
             src={images[0]}
             alt={title}
-            layout="fill"
-            objectFit="cover"
+            width={555}
+            height={416}
+            style={{ objectFit: 'cover' }}
             className="transform transition-transform duration-300 group-hover:scale-105"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
           {/* Gradient Overlay */}
           <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
