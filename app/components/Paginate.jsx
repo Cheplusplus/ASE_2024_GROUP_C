b3a44284c58205d3e5457971e9c58c5389dbd318
Karabo M. Radebe <kaymesuliradebe@gmail.com>
2024-10-29 10:36:25 +0200

    refactored the pagination to handle the dynamic changes

--- app/components/Paginate.jsx
+++ app/components/Paginate.jsx
@@ -1,32 +1,42 @@
 // app/components/Paginate.js
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import { useRouter } from 'next/navigation';
 
-const Paginate = ({ skip }) => {
-  const router = useRouter();
+const RECIPES_PER_PAGE = 52; // Recipes per page
 
-  const handleNext = () => {
+const Paginate = ({ skip, totalRecipes }) => {
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1); // Track current page
+
+  const handleNext = (newPage) => {
     const newSkip = skip + 52;
+    setCurrentPage(newPage);
     router.push(`/?skip=${newSkip}`); // Update the URL with the new skip value
   };
 
-  const handlePrevious = () => {
+  const handlePrevious = (newPage) => {
     const newSkip = skip > 52 ? skip - 52 : 0;
+    setCurrentPage(newPage);
     router.push(`/?skip=${newSkip}`);
   };
+
+  const totalPages = Math.ceil(totalRecipes / RECIPES_PER_PAGE);
 
   return (
     <div className="flex justify-center mt-8">
       <button
-        onClick={handlePrevious}
+        onClick={() => handlePrevious(currentPage - 1) }
         disabled={skip === 0}
         className="px-4 py-2 mx-2 text-white bg-gray-500 rounded disabled:opacity-50"
       >
         Previous
       </button>
+      <span className="px-4 py-2 mx-2 text-gray-700">
+        Page {currentPage} of {totalPages}
+      </span>
       <button
-        onClick={handleNext}
+        onClick={() => handleNext(currentPage + 1)}
         className="px-4 py-2 mx-2 text-white bg-gray-900 rounded"
       >
         Next
