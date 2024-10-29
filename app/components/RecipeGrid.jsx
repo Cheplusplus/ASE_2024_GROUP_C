2e5a063980c0ce968c2747e78504a981b0f83b2a
Karabo M. Radebe <kaymesuliradebe@gmail.com>
2024-10-29 09:55:55 +0200

    fetched the recipes via dot notation

--- app/components/RecipeGrid.jsx
+++ app/components/RecipeGrid.jsx
@@ -23,7 +23,7 @@ const fetchRecipes = async (skip= 0) => {
 const RecipeGrid = async ({ skip }) => {
   const recipes = await fetchRecipes(skip);
   
-  if (!recipes || recipes.length === 0) {
+  if (!recipes.recipes || recipes.length === 0) {
     return <SkeletonGrid />;
   }
 
@@ -36,7 +36,7 @@ const RecipeGrid = async ({ skip }) => {
         </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {recipes.map((recipe) => (
+        {recipes.recipes.map((recipe) => (
           <RecipeCard key={recipe._id} recipe={recipe} />
         ))}
       </div>
