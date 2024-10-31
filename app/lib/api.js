ce07274be713fdd8b26ddfa6f053bd78b9a482d7
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-10-31 16:39:11 +0200

    cache

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,6 +1,6 @@
 
 export async function getRecipes({ category, tags, numSteps, ingredients, sortOption ,skip ,search }) {
-  const res = await fetch(`http://localhost:3000/api/recipe?search=${search}&skip=${skip}&category=${category}&tags=${tags.join(',')}&numSteps=${numSteps}&ingredients=${ingredients}&sortOption=${sortOption}`);
+  const res = await fetch(`http://localhost:3000/api/recipe?search=${search}&skip=${skip}&category=${category}&tags=${tags.join(',')}&numSteps=${numSteps}&ingredients=${ingredients}&sortOption=${sortOption}`,{cache:'no-cache'});
   console.log(res.status)
   if (!res.ok) throw new Error('Failed to fetch recipes');
   const data = await res.json();
