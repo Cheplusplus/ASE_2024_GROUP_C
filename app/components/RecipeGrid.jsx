af005404089b759ff5c8dd78c9d4033dad719acb
Karabo M. Radebe <kaymesuliradebe@gmail.com>
2024-11-01 08:54:59 +0200

    removed the encodedURIcomponent

--- app/components/RecipeGrid.jsx
+++ app/components/RecipeGrid.jsx
@@ -5,10 +5,20 @@ import RecipeCard from './RecipeCard';
 import SkeletonGrid from './SkeletonMain';
 import Paginate from './Paginate';
 
+/**
+ * Fetches recipes from the API, using the skip parameter for pagination.
+ * 
+ * @async
+ * @function fetchRecipes
+ * @param {number} [skip=0] - The number of recipes to skip for pagination.
+ * @returns {Promise<Array>} - A promise that resolves to an array of recipe objects.
+ * @throws Will throw an error if the network request fails.
+ */
+
 const fetchRecipes = async (search = '', skip= 0) => {
   const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
   try {
-    const response = await fetch(`${url}/api/recipe/?search=${encodeURIComponent(search)}&skip=${skip}`, { cache: 'no-store' });
+    const response = await fetch(`${url}/api/recipe/?search=${(search)}&skip=${skip}`, { cache: 'no-store' });
     if (!response.ok) {
       throw new Error('Failed to fetch recipes');
     }
@@ -23,13 +33,11 @@ const fetchRecipes = async (search = '', skip= 0) => {
   }
 };
 
-const RecipeGrid = async ({ search, skip }) => {
-  // const {search = '', skip}  = searchParams
-  const recipes= await fetchRecipes(search || '', skip);
-  console.log(recipes.recipes)
-  if (!recipes || recipes.length=== 0) {
-    console.log( 'No recipes found')
-    
+const RecipeGrid = async ({ search , skip }) => {
+  
+  const recipes = await fetchRecipes(search || '', skip);
+  
+  if (!recipes || recipes.length === 0) {
     return <SkeletonGrid />;
   }
 
@@ -43,8 +51,10 @@ const RecipeGrid = async ({ search, skip }) => {
           Discover our collection of easy-to-make recipes that are perfect for any occasion.
         </p>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {recipes.recipes.map((recipe) => (
+
+      {/* Recipe Grid */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-2 sm:gap-8">
+        {recipes.recipes.map(recipe => (
           <RecipeCard key={recipe._id} recipe={recipe} />
         ))}
       </div>
