d4887eb6efb2a731c04945b0a94fb36ad341e085
Kitso Mogale <kitsomogale19@gmail.com>
2024-11-04 10:29:15 +0200

    .

--- app/api/recipe/route.js
+++ app/api/recipe/route.js
@@ -8,7 +8,7 @@ import { NextResponse } from "next/server";
  * @returns
  */
 
-export const dynamic = 'force-dynamic';  // Add this line to handle dynamic rendering
+// export const dynamic = 'force-dynamic';  // Add this line to handle dynamic rendering
 
 export async function GET(req) {
   try {
