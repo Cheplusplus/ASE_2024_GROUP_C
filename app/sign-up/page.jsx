8918e8e3e5bfb68c99c84e60f2bc5171e20d8592
phillip-tech <pm.bogopane@gmail.com>
2024-11-05 10:42:35 +0200

    Added sign-up page with imports and function declarations

---
+++ app/sign-up/page.jsx
@@ -0,0 +1,6 @@
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { signUpWithEmailAndPassword, signUpWithGoogle } from '../utils/auth'; // Mock functions for signup
+
