c1e03ca0f57c45be831a8df3e99c53fe2eda941c
phillip-tech <pm.bogopane@gmail.com>
2024-11-05 10:29:55 +0200

    Added sign-in page with imports and function declarations

---
+++ app/sign-in/page.jsx
@@ -0,0 +1,6 @@
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { signInWithEmailAndPassword, signInWithGoogle } from '../utils/auth'; // Mock functions for signin
+
