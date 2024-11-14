218ad707fa980b2aedc6a9650419103f6cff1385
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-11-14 14:09:45 +0200

    Aligned the loading state with the layout

--- app/loading.js
+++ app/loading.js
@@ -1,8 +1,13 @@
 const SkeletonGrid = () => {
     // console.log('12345678ioiuygfv')
     return (
-        <div className=" min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+        <div className=" min-h-screen  font-[family-name:var(--font-geist-sans)]">
+          <div className="bg-gray-700 h-64 w-full rounded-md"></div>
+            {/* Text Skeleton */}
+            <div className="space-y-2">
+              
+            </div>
+      <div className="grid grid-cols-2  sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6 p-4">
         {/* Creating 8 skeletons to mimic the recipe cards */}
         {Array(30).fill("").map((_, index) => (
           <div
