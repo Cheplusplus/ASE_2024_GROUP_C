63c4e469bbcc11257ddf21d817520c49e06b96de
Kitso Mogale <kitsomogale19@gmail.com>
2024-11-15 09:01:31 +0200

    .

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import UserSession from "./components/UserSession";
 
 export default function Home({searchParams}) {
 
-  
-
  return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 via-white to-gray-50">
       <main className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16">
@@ -27,3 +25,5 @@ export default function Home({searchParams}) {
     </div>
   );
 }
+
+
