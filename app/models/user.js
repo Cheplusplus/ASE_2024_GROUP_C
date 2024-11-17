4411cbe7d1b36cc2c4f963e212ccc55dbe7b12bd
Kitso Mogale <kitsomogale19@gmail.com>
2024-11-17 16:03:46 +0200

    udjusted users schema

--- app/models/user.js
+++ app/models/user.js
@@ -3,9 +3,8 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String}
+  username: { type: String, required: true, unique: true },
 });
 
-
-const User = mongoose.models.User || mongoose.model("User", UserSchema, "users");
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 export default User;
