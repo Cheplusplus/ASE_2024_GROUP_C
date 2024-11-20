fe1668d61e97bfe5d4239a921f604d12e18d568b
phillip-tech <pm.bogopane@gmail.com>
2024-11-20 09:21:23 +0200

    Added next-pwa configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  });
+  
+  const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'img.sndimg.com',
-                pathname: '/**'
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.sndimg.com',
+          pathname: '/**'
+        }
+      ]
+    },
+    // Add any other Next.js config options here
+  };
+  
+  export default withPWA(nextConfig);
