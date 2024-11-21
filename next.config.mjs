79baee5f14f59dc23e3ec2763f6ad9b8efc50530
phillip-tech <pm.bogopane@gmail.com>
2024-11-21 11:39:18 +0200

    Refactored next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-  });
+import withPWA from 'next-pwa'
+
+
   
   const nextConfig = {
     images: {
@@ -18,5 +15,12 @@ const withPWA = require('next-pwa')({
     },
     
   };
+
+  const pwaConfig = withPWA({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  });
   
-  export default withPWA(nextConfig);
+  export default pwaConfig(nextConfig);
