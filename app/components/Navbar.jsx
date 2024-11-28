ce6673a43f8b36986e245354492e1cc21dbe2b86
Karabo M. Radebe <kaymesuliradebe@gmail.com>
2024-11-28 11:48:59 +0200

    updated Navbar icons

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 import { signOut } from 'next-auth/react';
 import { ThemeToggle } from './ThemeToggle';
 import { useSession } from 'next-auth/react';
-
+import { Heart } from "lucide-react" 
 import { ShoppingCartIcon } from "lucide-react";
 import Image from "next/image";
 /**
@@ -230,10 +230,8 @@ useEffect(() => {
 
             {/* Shopping cart, Theme Toggle and Search */}
             <div className="flex items-center ">
-            <Link href="/favorites" className=" hidden md:block relative" >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                  <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-                </svg>
+            <Link href="/favorites" className=" hidden md:block relative p-2 rounded-md text-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" >
+                <Heart />
                 {favouritesCount > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-2 py-1">
                     {favouritesCount}
