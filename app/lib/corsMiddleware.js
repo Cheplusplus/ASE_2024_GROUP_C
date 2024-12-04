d69007c07ba65a04d0a276e3cca49e6f15db6a77
Kitso Mogale <kitsomogale19@gmail.com>
2024-12-04 23:54:05 +0200

    "*"

--- app/lib/corsMiddleware.js
+++ app/lib/corsMiddleware.js
@@ -3,7 +3,7 @@
 export function setCORSHeaders(req) {
   const headers = new Headers();
 
-  headers.set("Access-Control-Allow-Origin", "https://ase-2024-group-c.vercel.app");
+  headers.set("Access-Control-Allow-Origin", "*");
   headers.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   headers.set("Access-Control-Allow-Credentials", "true");
